f16c7727e432772fbf405b0d42f8cf2e20e676a7
uldahlalex <41024316+uldahlalex@users.noreply.github.com>
2022-11-10 12:13:50 +0100

    Update webserver.js

--- frontend/webserver.js
+++ frontend/webserver.js
@@ -2,14 +2,15 @@ const express = require('express');
 const port = 8100;
 const app = express();
 const nocache = require("nocache");
+const path = require("path");
 
-
-app.use(express.static('dist'))
 app.use(nocache())
+app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('*', (req, res) => {
-  res.sendFile('index.html',{root:__dirname+'/dist/'})
-});
+// PathLocationStrategy
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+})
 app.listen(process.env.PORT || port, () => {
   console.log("Server is listening on port "+port);
 });
