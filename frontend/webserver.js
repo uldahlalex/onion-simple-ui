560e3f8fc54752475047a8b64aa082a644ffe212
uldahlalex <41024316+uldahlalex@users.noreply.github.com>
2022-11-11 12:39:24 +0100

    deployment

--- frontend/webserver.js
+++ frontend/webserver.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const port = 8100;
 const app = express();
-const nocache = require("nocache");
 const path = require("path");
 
-app.use(nocache())
 app.use(express.static(__dirname + '/dist/frontend'));
 
 // PathLocationStrategy
